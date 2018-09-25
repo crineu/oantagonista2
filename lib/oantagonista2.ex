6682226a6d225c97be136b67868db40a954bb866
Crineu Tres <crineu@gmail.com>
2018-09-24 22:47:47 -0300

    mix new oantagonista2; elixir 1.7

---
+++ lib/oantagonista2.ex
@@ -0,0 +1,18 @@
+defmodule Oantagonista2 do
+  @moduledoc """
+  Documentation for Oantagonista2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Oantagonista2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/oantagonista2_test.exs
@@ -0,0 +1,8 @@
+defmodule Oantagonista2Test do
+  use ExUnit.Case
+  doctest Oantagonista2
+
+  test "greets the world" do
+    assert Oantagonista2.hello() == :world
+  end
+end
