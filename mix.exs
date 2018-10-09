8cf7f60975c132256d34248a9e7c077af786d57f
Crineu Tres <crineu@gmail.com>
2018-10-08 23:32:58 -0300

    Added code coverage with `excoveralls`
    `mix coveralls` for simple output
    `mix coveralls.html` for a html page

--- mix.exs
+++ mix.exs
@@ -9,6 +9,13 @@ defmodule Oantagonista2.MixProject do
 
       escript: escript_config(),
       deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls:          :test,
+        "coveralls.detail": :test,
+        "coveralls.post":   :test,
+        "coveralls.html":   :test
+      ],
       start_permanent: Mix.env() == :prod,
 
       # Docs
@@ -37,8 +44,9 @@ defmodule Oantagonista2.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:floki,  "~> 0.20.0"},
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+      {:floki,       "~> 0.20.0"},
+      {:ex_doc,      "~> 0.19", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
     ]
   end
 
