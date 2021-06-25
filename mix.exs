481e49a1d22e51712b3f2745ec95c22b235d3add
Crineu Tres <crineu@gmail.com>
2021-06-25 17:03:24 -0300

    Upgrade heroku stack to heroku-20

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Oantagonista2.MixProject do
   def project do
     [
       app:  :oantagonista2,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir:  "~> 1.7",
 
       escript: escript_config(),
